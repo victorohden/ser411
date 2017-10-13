1292a70967b189a40fdee8cb13e78bb8781480d8
Victor Hugo Rohden Prudente <victor.rohden@yahoo.com>
2017-10-13 17:58:35 -0300

    Add files via upload

--- computa-grade-focos.py
+++ computa-grade-focos.py
@@ -1,9 +1,8 @@
-# encoding: utf-8
-
 import sys
-
+# Carrega a biblioteca OS que fornece funcionalidades
+# associadas ao Sistema Operacional
 import os
-
+# Carrega a biblioteca NumPy para criação de matrizes
 import numpy as np
 
 try:
@@ -17,7 +16,9 @@ gdal.UseExceptions()
 ogr.UseExceptions()
 osr.UseExceptions()
 
-vector_file = "/home/labgeo4/Dados/Queimadas/focos/focos-2016.shp"
+#Definição de constantes globais do programa
+#vector_file = "/home/labgeo4/Dados/Queimadas/focos/focos-2016.shp"
+vector_file = r"F:\OneDrive\Doutorado\Materias\PDI2\python\2016\focos\focos-2016.shp"
 vector_file_base_name = os.path.basename(vector_file)
 layer_name = os.path.splitext(vector_file_base_name)[0]
 spatial_extent = {'xmin':-89.975, 'ymin':-59.975,'xmax':-29.975, 'ymax':10.025}
@@ -25,18 +26,21 @@ spatial_resolution = {'x':0.05, 'y':0.05}
 grid_dimensions = {'cols': 1200, 'rows': 1400}
 
 file_format = "GTiff"
-outup_file_name = "/home/labgeo4/Dados/Queimadas/focos/grade-2016.tif"
+outup_file_name = r"F:\OneDrive\Doutorado\Materias\PDI2\python\2016\focos\grade-2016.tif"
 shp_focos = ogr.Open(vector_file)
 
+#Abrindo arquivo vetorial com focos de queimada
 if shp_focos is None:
     sys.exit("Erro: não foi possivel abrir o arquivo '{0}'.".format(vector_file))
 layer_focos = shp_focos.GetLayer(layer_name)
 
 if layer_focos is None:
-    sys.exit("Erro: não foi possivel abrir o arquivo '{0}' no arquivo {1}!.".format(layer_name, vector_file))
+    sys.exit("Erro: não foi possivel abrir o arquivo '{0}' no arquivo {1}!".format(layer_name, vector_file))
 
+#Criando uma matriz numérica
 matriz = np.zeros((grid_dimensions['rows'], grid_dimensions['cols']), np.int16)
 
+#Calculando o número de focos associado a cada célula
 for foco in layer_focos:
     location = foco.GetGeometryRef()
 
@@ -44,8 +48,8 @@ for foco in layer_focos:
 
     matriz[row, col] += 1
 
+#Criando o raster de saida
 driver = gdal.GetDriverByName(file_format)
-
 if driver is None:
     sys.exit("Erro: não foi possivel identificar o driver '{0}'.".format(file_format))
 
@@ -63,3 +67,6 @@ band = raster.GetRasterBand(1)
 band.WriteArray(matriz,0,0)
 
 band.FlushCache()
+
+raster = None
+del raster, band
